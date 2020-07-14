918349d88d78fa365e7e3be40224d6aca6fee09a
Quan Thai <nguyenvinhhuy542@gmail.com>
2020-07-14 21:27:29 +0700

    'buoi2_state_renderwithmap'

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Header from './Components/Header';
 import Baitaplayout from './Components/BaiTapLayout/Baitaplayout';
 import BT2layout from './Components/BTlayout2/BT2layout';
 import Databinding from './Components/DataBase/Databinding';
+import Evenbinding from './EvenBinding/Evenbinding';
+import RenderwithStale from './RenderWithStale/renderwithStale';
+import Baitapchonxe from './RenderWithStale/Baitapchonxe';
+import RenderMap from './RenderMap/RenderMap';
 
 // App như hàm main: mọi component đều hiển thị qua App
 function App() {
   return (
     <div className="App">
-     
-      <Databinding></Databinding>
+      {/* <Baitapchonxe></Baitapchonxe> */}
+      <RenderMap></RenderMap>
+      {/* <RenderwithStale></RenderwithStale> */}
+     {/* <Evenbinding></Evenbinding> */}
+      {/* <Databinding></Databinding> */}
     </div>
   )
 }
